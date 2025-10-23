831ebf902b0cee76c47fbde5b0b688b2bf3dccb1
Brian Maleek <maleekmalik@gmail.com>
2025-10-24 00:21:30 +0300

    Add ErrorProneComponent to simulate errors for testing purposes

---
+++ alx-rick-and-morty-app/components/ErrorProneComponent.tsx
@@ -0,0 +1,6 @@
+const ErrorProneComponent: React.FC = () => {
+    // Simulate an error for demonstration purposes
+    throw new Error("This is a test error!");
+};
+
+export default ErrorProneComponent;
